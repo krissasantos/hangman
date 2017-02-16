e96a1cbf4ba13659470e9f20499362657ce64e86
krissasantos <krissa.santos31@gmail.com>
2017-02-16 17:16:17 -0500

    added game_time method which includes gsub loop working. issue: fix strike system to successfully break

--- api.rb
+++ api.rb
@@ -3,7 +3,6 @@
 require 'httparty'
 require 'byebug'
 
-byebug
 
 def get_words
   response = HTTParty.get('http://linkedin-reach.hagbpyjegb.us-west-2.elasticbeanstalk.com/words')
@@ -13,15 +12,17 @@ end
 
 #Ideally: 
 #Create hash with words as keys and values including the optional parameters: difficulty, minilength, maxlenght,start, count (review structure later)
-#For now, get basics working by:
 
+#For now, get basics working by:
 #Step 1: computer collecting words
 words = get_words.split(" ")
 
 # def start_game
 p "Hello! How long should the word you're guessing be?"
-length = gets.chomp()
+length = gets.chomp().to_i
+"Pick your game level from 1-10:"
 
+difficulty = gets.chomp().to_i
 # end
 
 def pick_a_word(length)
@@ -38,12 +39,55 @@ def pick_a_word(length)
   chosen_word = filtered_wrds_by_length.sample
 
 end
-pick_a_word(length)
+chosen_word = pick_a_word(length)
+
+
+
+
+#Rules
+# User can keep guessing until they reach the number of strikes
+# strikes may be provided at the start of the game. in that case, 
+# change 5 into the strikes variable based on difficulty chosen by user 
+# in the beginning
+
+
+def game_time(chosen_word)
+  strikes = 0
+  blanks = "X" * chosen_word.length 
+
+  until strikes == 5 #|| !blanks.include?("X")
+    p "Give me a letter:"
+    letter = gets.chomp()
+
+    if chosen_word.include?(letter)
+      chosen_word.each_char.with_index do |char, idx|
+        if char == letter
+            blanks[idx] = letter
+            p blanks
+        end
+      end
+
+    else
+      byebug
+      strikes +=1
+      p "Nope, try again."
+     
+    end
+  end
+
+  if strikes == 5 #or strikes variable 
+   p "You lost. The word is #{chosen_word}"#.Want to play again? y/n"#insert "Want to play again?"
+    # play_again = gets.chomp()
+    # if play_again == "y" ||play_again == "Y" 
+      
+    # end
+  elsif  !blanks.include?("X")
+   p "Good job!" #insert "Want to play again?"
+  end
+end
+
 
 p "hola!"
-
-
-
 
 
 
