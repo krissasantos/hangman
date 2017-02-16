c4b88402cff91936a3e9ae349d74c10dfd392e58
krissasantos <krissa.santos31@gmail.com>
2017-02-15 22:15:52 -0500

    filtered words and sampling with chosen length -  successful

--- api.rb
+++ api.rb
@@ -3,13 +3,49 @@
 require 'httparty'
 require 'byebug'
 
-
+byebug
 
 def get_words
- 
   response = HTTParty.get('http://linkedin-reach.hagbpyjegb.us-west-2.elasticbeanstalk.com/words')
   # converted_response = response
   converted_response = response.parsed_response
 end
 
+#Ideally: 
+#Create hash with words as keys and values including the optional parameters: difficulty, minilength, maxlenght,start, count (review structure later)
+#For now, get basics working by:
+
+#Step 1: computer collecting words
 words = get_words.split(" ")
+
+# def start_game
+p "Hello! How long should the word you're guessing be?"
+length = gets.chomp()
+
+# end
+
+def pick_a_word(length)
+  #remove sandwich code and change into map after it gets working
+  words = get_words.split(" ")
+  filtered_wrds_by_length = []
+  
+  words.each do |wrd|
+    if wrd.size == length.to_i
+      filtered_wrds_by_length << wrd
+    end
+  end
+
+  chosen_word = filtered_wrds_by_length.sample
+
+end
+pick_a_word(length)
+
+p "hola!"
+
+
+
+
+
+
+
+
